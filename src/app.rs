b7ae1a939ab16a7a661f74d5b6e62953fc6460c8
Christian Smith <christianryansmith@gmail.com>
2026-05-11 14:02:46 -0500

    wip: add rectangle

--- src/app.rs
+++ src/app.rs
@@ -7,15 +7,39 @@ pub struct WorkflowApp {
 }
 
 impl WorkflowApp {
-    pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
-        let initial_features = vec![Feature::Rectangle { x: 20, y: 20, w: 10,h: 10 }];
-        Self { features:  }
+    pub fn new(_window: &mut Window, cx: &mut Context<Self>) -> Self {
+        let initial_features = vec![Feature::Rectangle {
+            x: 20.,
+            y: 20.,
+            w: 10.,
+            h: 10.,
+        }];
+        Self {
+            features: initial_features
+                .into_iter()
+                .map(|feature| cx.new(|_cx| feature))
+                .collect(),
+        }
     }
 }
 
 impl Render for WorkflowApp {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         // PLACEHOLDER FOR UI
-        div().child("Hello world")
+        div()
+            .child("Hello world")
+            .children(
+                self.features
+                    .clone()
+                    .iter()
+                    .map(|feature| match feature.read(cx) {
+                        Feature::Rectangle { x, y, w, h } => {
+                            div().child(format!("RECTANGLE: {} {} {} {}", x, y, w, h))
+                        }
+                        Feature::Circle { x, y, r } => {
+                            div().child(format!("CIRCLE {} {} {}", x, y, r))
+                        }
+                    }),
+            )
     }
 }
