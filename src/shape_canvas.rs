4897de7936acf091eaea343968611f11eff55005
Christian Smith <christianryansmith@gmail.com>
2026-05-13 01:26:04 -0500

    wip: changed scroll to be pan not zoom

--- src/shape_canvas.rs
+++ src/shape_canvas.rs
@@ -97,21 +97,23 @@ impl RenderOnce for ShapeCanvas {
                 let delta = event.delta.pixel_delta(px(1.));
 
                 state_for_wheel.update(cx, |state, _| {
-                    let camera_zoom_prev = state.camera_zoom;
+                    // let camera_zoom_prev = state.camera_zoom;
 
-                    state.camera_zoom += delta.y.as_f32() / 40.;
-                    state.camera_zoom = state.camera_zoom.clamp(1., 10.);
+                    state.camera_x -= delta.x.as_f32() * state.camera_zoom;
+                    state.camera_y -= delta.y.as_f32() * state.camera_zoom;
 
-                    let dcx = event.position.x * (camera_zoom_prev - state.camera_zoom);
-                    let dcy = event.position.y * (camera_zoom_prev - state.camera_zoom);
+                    // state.camera_zoom += delta.y.as_f32() / 40.;
+                    // state.camera_zoom = state.camera_zoom.clamp(1., 10.);
 
-                    state.camera_x += dcx.as_f32();
-                    state.camera_y += dcy.as_f32();
+                    // let dcx = event.position.x * (camera_zoom_prev - state.camera_zoom);
+                    // let dcy = event.position.y * (camera_zoom_prev - state.camera_zoom);
+
+                    // state.camera_x += dcx.as_f32();
+                    // state.camera_y += dcy.as_f32();
                 });
                 cx.notify(state_for_wheel.entity_id());
             })
             .on_pinch(move |event, _, cx| {
-                println!("On pinch! : {:?}", event.delta);
                 let delta = event.delta;
 
                 state_for_pinch.update(cx, |state, _| {
