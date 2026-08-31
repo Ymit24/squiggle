fb87155706830e229bc99adaa2befb042d5259e4
Christian Smith <christianryansmith@gmail.com>
2026-08-31 17:01:51 -0400

    refactor: remove nextId from Document

--- squiggle_flutter/packages/data_models/lib/src/document.dart
+++ squiggle_flutter/packages/data_models/lib/src/document.dart
@@ -5,34 +5,27 @@ import 'node.dart';
 const dataModelFormatVersion = 1;
 
 final class Document {
-  const Document({
-    this.name = 'Untitled',
-    this.nextId = 1,
-    this.nodes = const [],
-  });
+  const Document({this.name = 'Untitled', this.nodes = const []});
 
   final String name;
-  final int nextId;
   final List<Node> nodes;
 
   factory Document.fromJson(Map<String, dynamic> json) {
     if (json['version'] != dataModelFormatVersion) {
       throw FormatException('Unsupported document version: ${json['version']}');
     }
-    return Document(
-      name: json['name'] as String? ?? 'Untitled',
-      nextId: json['nextId'] as int? ?? 1,
-      nodes: [
-        for (final node in json['nodes'] as List<dynamic>)
-          Node.fromJson(node as Map<String, dynamic>),
-      ],
-    );
+
+    final nodes = [
+      for (final node in json['nodes'] as List<dynamic>)
+        Node.fromJson(node as Map<String, dynamic>),
+    ];
+
+    return Document(name: json['name'] as String? ?? 'Untitled', nodes: nodes);
   }
 
   Map<String, dynamic> toJson() => {
     'version': dataModelFormatVersion,
     'name': name,
-    'nextId': nextId,
     'nodes': nodes.map((node) => node.toJson()).toList(),
   };
 
