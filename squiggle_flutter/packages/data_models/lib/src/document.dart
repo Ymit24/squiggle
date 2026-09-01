4eb13e1075725b2f32a7430c6f641b29f2ff7302
Christian Smith <christianryansmith@gmail.com>
2026-08-31 20:02:22 -0400

    wip: remove uneeded name from raw data model document

--- squiggle_flutter/packages/data_models/lib/src/document.dart
+++ squiggle_flutter/packages/data_models/lib/src/document.dart
@@ -5,9 +5,8 @@ import 'node.dart';
 const dataModelFormatVersion = 1;
 
 final class Document {
-  const Document({this.name = 'Untitled', this.nodes = const []});
+  const Document({this.nodes = const []});
 
-  final String name;
   final List<Node> nodes;
 
   factory Document.fromJson(Map<String, dynamic> json) {
@@ -20,12 +19,11 @@ final class Document {
         Node.fromJson(node as Map<String, dynamic>),
     ];
 
-    return Document(name: json['name'] as String? ?? 'Untitled', nodes: nodes);
+    return Document(nodes: nodes);
   }
 
   Map<String, dynamic> toJson() => {
     'version': dataModelFormatVersion,
-    'name': name,
     'nodes': nodes.map((node) => node.toJson()).toList(),
   };
 
