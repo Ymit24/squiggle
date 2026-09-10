c5c5fce5bccb09ae7d8e02b81da1a3a64f37a296
Christian Smith <christianryansmith@gmail.com>
2026-09-09 21:20:07 -0400

    wip: address feedback

--- squiggle_flutter/lib/editor/history/history.dart
+++ squiggle_flutter/lib/editor/history/history.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:squiggle_flutter/editor/history/edit.dart';
 import 'package:squiggle_flutter/models/document.dart';
 import 'package:squiggle_flutter/models/node.dart';
@@ -13,7 +14,8 @@ class History {
 
   History(this._document);
 
-  Transaction? get active => _active;
+  Transaction get active => _active!;
+  bool get isActive => _active != null;
 
   void begin(String label, {NodeContainer? container}) {
     if (_active != null) {
@@ -27,22 +29,35 @@ class History {
     );
   }
 
+  bool get canUndo => _undoStack.isNotEmpty;
+  bool get canRedo => _redoStack.isNotEmpty;
+
   void undo() {
     if (_undoStack.isEmpty) {
       throw StateError('No commits to undo');
     }
+    if (_active != null) {
+      throw StateError('Cannot undo while a transaction is active');
+    }
 
-    final commit = _undoStack.removeLast();
+    final commit = _undoStack.last;
     commit.undo(_document);
+    _undoStack.removeLast();
+    _redoStack.add(commit);
   }
 
   void redo() {
     if (_redoStack.isEmpty) {
       throw StateError('No commits to redo');
     }
+    if (_active != null) {
+      throw StateError('Cannot redo while a transaction is active');
+    }
 
-    final commit = _redoStack.removeLast();
+    final commit = _redoStack.last;
     commit.redo(_document);
+    _redoStack.removeLast();
+    _undoStack.add(commit);
   }
 
   void commit() {
@@ -53,6 +68,7 @@ class History {
     final commit = _active!.commit();
     if (commit != null) {
       _undoStack.add(commit);
+      _redoStack.clear();
     }
 
     _active = null;
@@ -73,7 +89,12 @@ class History {
     NodeContainer? container,
   }) {
     begin(label, container: container);
-    action(_active!);
-    commit();
+    try {
+      action(_active!);
+      commit();
+    } catch (_) {
+      cancel();
+      rethrow;
+    }
   }
 }
