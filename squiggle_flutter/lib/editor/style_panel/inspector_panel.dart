1ab0845a70a71b6f6ec4de2b65d9bc66bfa8910f
Squiggle <squiggle@local>
2026-09-21 00:10:48 +0000

    refactor: extract inspector fields widget

--- squiggle_flutter/lib/editor/style_panel/inspector_panel.dart
+++ squiggle_flutter/lib/editor/style_panel/inspector_panel.dart
@@ -1,8 +1,7 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:squiggle_flutter/editor/editor_context.dart';
+import 'package:squiggle_flutter/editor/style_panel/widgets/inspector_fields.dart';
 import 'package:squiggle_flutter/models/feature.dart';
-import 'package:squiggle_flutter/models/feature_kinds/inspector_field.dart';
 import 'package:squiggle_flutter/theme/squiggle_theme.dart';
 
 class InspectorPanel extends StatelessWidget {
@@ -26,12 +25,6 @@ class InspectorPanel extends StatelessWidget {
             .whereType<Feature>();
         if (selectedNodes.isEmpty) return SizedBox.shrink();
 
-        final inspectorFieldWidgets = _buildInspectorFields(
-          context,
-          editorContext,
-          selectedNodes.toList(),
-        );
-
         print("Building inspector.");
 
         return DecoratedBox(
@@ -40,19 +33,9 @@ class InspectorPanel extends StatelessWidget {
             borderRadius: BorderRadius.circular(theme.radii.floatingPanel),
             child: SingleChildScrollView(
               padding: EdgeInsets.all(spacing.panelPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ...inspectorFieldWidgets
-                      .map(
-                        (widget) => [
-                          widget,
-                          SizedBox(height: spacing.panelSectionSpacing),
-                        ],
-                      )
-                      .flattened,
-                ],
+              child: InspectorFields(
+                editorContext: editorContext,
+                features: selectedNodes.toList(),
               ),
             ),
           ),
@@ -60,20 +43,4 @@ class InspectorPanel extends StatelessWidget {
       },
     );
   }
-}
-
-Iterable<InspectorFieldShell> _buildInspectorFields(
-  BuildContext context,
-  EditorContext editorContext,
-  List<Feature> features,
-) {
-  final inspectorFieldByKey = InspectorField.byKeyForFeatures(features);
-  return inspectorFieldByKey.values.map(
-    (field) => field.build(context, (result) {
-      editorContext.history.run('Inspector Update', (transaction) {
-        transaction.watch(features);
-        field.apply(result);
-      });
-    }),
-  );
 }
